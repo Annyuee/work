ec1baa555ea207e7625233b25a2f22e4dfcf929d
User <user@example.com>
2026-03-20 21:26:56 +0800

    初始化项目

--- fix_password.sql
+++
@@ -1,7 +0,0 @@
-USE library_system;
-
--- 设置正确的 bcrypt 密码（123456 的加密值）
-UPDATE sys_user SET password = '$2a$10$N.zmdr9k7uOCQb376NoUnuTJ8iAt6Z5EHsM8lE9lBOsl7iKTVKIUi' WHERE username = 'admin';
-UPDATE sys_user SET password = '$2a$10$N.zmdr9k7uOCQb376NoUnuTJ8iAt6Z5EHsM8lE9lBOsl7iKTVKIUi' WHERE username = 'reader';
-
-SELECT username, password FROM sys_user;
